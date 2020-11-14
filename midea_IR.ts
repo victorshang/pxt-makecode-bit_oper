3b383eae21fb32d9db6d2c45876cf1bac651d75f
Victor Shang <30793693+victorshang@users.noreply.github.com>
2020-11-14 10:48:59 +0800

    更新 pxt.json, main.blocks, main.ts, midea_IR.ts

--- midea_IR.ts
+++ midea_IR.ts
@@ -15,26 +15,52 @@ enum wind_code {
 }
 
 enum tmp_code {
-    tmp_17=0,
-    tmp_18=1,
-    tmp_19=3,
-    tmp_20=2,
-    tmp_21=6,
-    tmp_22=7,
-    tmp_23=5,
-    tmp_24=4,
-    tmp_25=12,
-    tmp_26=13,
-    tmp_27=9,
-    tmp_28=8,
-    tmp_29=10,
-    tmp_30=11,
-    tmp_undef=14,
+    Tmp_17=0,
+    Tmp_18=1,
+    Tmp_19=3,
+    Tmp_20=2,
+    Tmp_21=6,
+    Tmp_22=7,
+    Tmp_23=5,
+    Tmp_24=4,
+    Tmp_25=12,
+    Tmp_26=13,
+    Tmp_27=9,
+    Tmp_28=8,
+    Tmp_29=10,
+    Tmp_30=11,
+    Tmp_undefine=14,
 }
 
 let A_code= 178
 
 namespace midea_ir{   
+
+    /**
+    * 返回开机码
+    */
+    //% block="开机码，模式：%mode=Cold_mode 温度:%tmp=Tmp_24 风量:%wind=High_wind"
+    export function getOpenCode(mode:mode_code, tmp:tmp_code, wind:wind_code):string{
+        let result="1011001001001101"
+        let codeB=getOpenCodeB(wind)
+        let codeC=getOpenCodeC(tmp, mode)
+        for(let i = 7; i >= 0; i--) {
+            result +=  convertToText(bitoperation.getBit(codeB, i))
+        }      
+        for(let i = 7; i >= 0; i--){
+            result+=convertToText(bitoperation.getBit(bitoperation.getInverse(codeB), i))
+        }
+        for(let i = 7; i >= 0; i--){
+            result+=convertToText(bitoperation.getBit(codeC, i))
+        }
+        for(let i = 7; i >= 0; i--){
+            result+=convertToText(bitoperation.getBit(bitoperation.getInverse(codeC), i))
+        }
+        return result
+        
+    }
+
+
     /**
     * 返回A码
     */
